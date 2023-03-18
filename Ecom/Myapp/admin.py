94b910c73834f7300aae14364abcb6569f39113c
middo1 <hamidadeshina1@gmail.com>
2023-03-18 17:09:40 +0100

    omoo i don try

--- Ecom/Myapp/admin.py
+++ Ecom/Myapp/admin.py
@@ -3,7 +3,8 @@ from django.contrib import admin
 # Register your models here.
 
 from .models import content
-from .models import product_content
+from .models import product_content, Cart
 
 admin.site.register(content)
 admin.site.register(product_content)
+admin.site.register(Cart)
